2f8a6028cccd1db9f04fac7e5753f5d14ed479bc
Carlos Freire <carhb@outlook.com>
2024-02-28 08:20:25 -0300

    Aula 3 "Terminado aula 3"

--- js/criarVideo.js
+++ js/criarVideo.js
@@ -1,6 +1,8 @@
-const formulario = document.querySelector("data-formulario")
+import { conectaApi } from "./conectaApi.js"
 
-function criarVideo(evento) {
+const formulario = document.querySelector("[data-formulario]")
+
+async function criarVideo(evento) {
     evento.preventDefault()
 
     const imagem = document.querySelector("[data-imagem]").value
@@ -8,5 +10,11 @@ function criarVideo(evento) {
     const titulo = document.querySelector("[data-titulo]").value
     const descricao = Math.floor(Math.random() * 10).toString()
 
-} formulario.addEventListener("submit", evento => criarVideo(evento))
+    await conectaApi.criaVideo(titulo, descricao, url, imagem)
+
+    window.location.href = "../pages/envio-concluido.html"
+
+} 
+
+formulario.addEventListener("submit", evento => criarVideo(evento))
 
