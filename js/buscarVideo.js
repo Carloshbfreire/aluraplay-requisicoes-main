b52b21e0e9bb3c75601842c7f64b6a57e8de7db0
Carlos Freire <carhb@outlook.com>
2024-03-10 15:15:18 -0300

    Terminado aula 5 "Lidando com os erros"

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -14,8 +14,12 @@ async function buscarvideo(evento) {
     }
 
     busca.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo,
-        elemento.descricao, elemento.url, elemento.imagem)))       
-    
+        elemento.descricao, elemento.url, elemento.imagem)))
+        
+    if (busca.length == 0) {
+        lista.innerHTML = `<h2 class="mensagem__titulo"> Não existem vídeos com esse termo</h2>`
+    }   
+              
 
 }
 
